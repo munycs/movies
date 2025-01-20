69ffd500486717432f067a3448e105fee9f7a656
munycs <munycs1@gmail.com>
2025-01-20 14:07:00 +0700

    change name

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import "../css/Navbar.css"
 function NavBar(){
     return <nav className="navbar">
         <div className="navbar-brand">
-            <Link to="/">Movie App</Link>
+            <Link to="/">Movie With Muny</Link>
         </div>
         <div className="navbar-links">
             <Link to="/" className="nav-link">Home</Link>
